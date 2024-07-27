2d8f385166636c787f10d64cb9aa49738cee3fb6
Prabha <prabhakaran.srinivasan@Relevantz.com>
2024-07-27 16:12:45 +0530

    first commit

---
+++ src/main/java/com/prabha/maventest/sum.java
@@ -0,0 +1,18 @@
+package com.prabha.maventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class sum 
+{
+	public static int SumOfDigits(int n) {
+		int sum = 0;
+		while (n != 0) {
+			int y = n % 10;
+			sum = sum + y;
+			n = n / 10;
+		}
+		return sum;
+	}
+}
---
+++ src/test/java/com/prabha/maventest/sumTest.java
@@ -0,0 +1,15 @@
+package com.prabha.maventest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class sumTest {
+
+	@Test
+	void testSumOfDigits() {
+		assertEquals(10,sum.SumOfDigits(55));
+
+	}
+
+}
